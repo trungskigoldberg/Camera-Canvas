fe2204ff6da412a02a71e00e675205bf71109076
Trung Ngo <trungthengo1995@gmail.com>
2016-07-24 21:25:55 -0400

    This adds new project of Camera Capture stuff, also adding some changes in CameraCanvasUI/Toolbox as well. Start the solution to execute the whole. Also, the only one wav sound file was corrupted before for unknown reason, and I just replaced a new sound for it.

---
+++ CameraCanvasUI/Toolbox/CameraCCButton.Designer.cs
@@ -0,0 +1,23 @@
+﻿namespace CameraCanvas.Toolbox
+{
+    partial class CameraCCButton
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CameraCanvasUI/Toolbox/CameraCCButton.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using CameraCaptureWithHAARCascade;
+
+namespace CameraCanvas.Toolbox
+{
+    public partial class CameraCCButton : CCButton
+    {
+        public CameraCCButton(CCToolbar ccToolBar)
+            :base(ccToolBar, "&Camera", "Camera", "Icons/Photo.png")
+        {
+            this.Click += new EventHandler(CameraCCButton_Click);
+        }
+
+        void CameraCCButton_Click(object sender, EventArgs e)
+        {
+            CameraCapture cc = new CameraCapture();
+            cc.ShowDialog();
+        }
+
+
+    }
+}
